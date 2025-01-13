dfbb3a248d483a37ad6569eec8172df3441739c8
Dio <doibncm2003@gmail.com>
2025-01-13 09:26:16 +0700

    closures

--- closures/index.go
+++ closures/index.go
@@ -2,26 +2,37 @@ package main
 
 import "fmt"
 
-//Tạo hàm mới intSeq bao một hàm khác 
-//bên trong và đóng biến tạo thành 1 closure
+// Tạo hàm intSeq trả về một hàm con (closure).
+// Hàm intSeq không trả về một giá trị cố định, mà trả về một hàm khác.
+// Hàm con này có thể truy cập và thay đổi biến i được khai báo trong hàm intSeq.
 func intSeq() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
-	}
+    // Khai báo biến i và gán giá trị ban đầu là 0
+    i := 0
+
+    // Trả về một hàm ẩn danh (anonymous function) có thể thay đổi giá trị của i
+    return func() int {
+        i++ // Tăng giá trị của i lên 1 mỗi lần hàm con được gọi
+        return i // Trả về giá trị của i sau khi tăng
+    }
 }
+
 func main() {
 
-	nextInt := intSeq()
+    // Gọi intSeq để nhận được một hàm con (closure) và gán vào biến nextInt
+    nextInt := intSeq()
 
-	//Mỗi lần in ra là một kq riêng biệt, giá trị này
-	//sẽ được cập nhật nếu cta gọi nextInt
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
+    // Mỗi lần gọi nextInt() sẽ nhận giá trị của biến i, sau đó i sẽ được tăng lên 1.
+    // Kết quả sẽ là 1, 2, 3 cho lần gọi tiếp theo
+    fmt.Println(nextInt()) // In ra: 1
+    fmt.Println(nextInt()) // In ra: 2
+    fmt.Println(nextInt()) // In ra: 3
 
-	newInts := intSeq()
-	fmt.Println(newInts())
-	fmt.Println(nextInt())
+    // Gọi lại intSeq để tạo ra một closure mới với biến i được khởi tạo lại từ 0
+    newInts := intSeq()
+
+    // nextInts sẽ trả về giá trị đầu tiên của closure mới (i = 1)
+    fmt.Println(newInts()) // In ra: 1
+
+    // nextInt vẫn giữ giá trị của biến i là 4 (tiếp tục từ lần gọi cuối cùng), vì nó là một closure riêng biệt
+    fmt.Println(nextInt()) // In ra: 4
 }
